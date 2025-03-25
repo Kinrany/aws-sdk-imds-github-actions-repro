831d55cb9d05629c7f898501a4017a2ea68e6362
Ruslan Fadeev <github@kinrany.dev>
2025-03-25 20:47:29 +0400

    load credentials

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::error::Error;
 
 use aws_config::BehaviorVersion;
+use aws_credential_types::provider::ProvideCredentials;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    aws_config::load_defaults(BehaviorVersion::latest()).await;
+    let sdk_config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+    let credentials_provider = sdk_config
+        .credentials_provider()
+        .ok_or("No credentials provider")?;
+    let _credentials = credentials_provider.provide_credentials().await?;
     Ok(())
 }
